3c9e6e50c1aa026fdccc12733ac14009e264ffbf
imjinbro <jinbro@likelion.org>
2018-07-19 15:27:43 +0900

    init project

---
+++ src/main/java/rentroomer/roomreview/RoomReviewApplication.java
@@ -0,0 +1,12 @@
+package rentroomer.roomreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomReviewApplication.class, args);
+    }
+}
---
+++ src/test/java/rentroomer/roomreview/RoomReviewApplicationTests.java
@@ -0,0 +1,16 @@
+package rentroomer.roomreview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoomReviewApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
